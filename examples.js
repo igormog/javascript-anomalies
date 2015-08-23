3a2db32023c2c56419c46f0276fa715d8845c0f4
Igor Mogilin <mogistein@yandex.ru>
2015-08-23 23:21:11 +0300

    edit html

--- examples.js
+++ examples.js
@@ -1,19 +1,24 @@
 // MIN_VALUE это наименьшее число, БОЛЬШЕ НУЛЯ
 Number.MIN_VALUE > 0; // true
 
+
 // null, хоть и имеет тип "object", не является Object'ом
 typeof null; // object
 null === Object; // false
 
+
 // no comment
 NaN === NaN; // false
+
 
 // NaN — not a number.
 typeof NaN; // number
 
+
 // Если не поставить ';', то интерпретируется как: var у = f(a+b).toString();
 var у = х + f
 (a+b) . toString( );
+
 
 // Проверка со­держит ли переменная х значение NaN:
 x == NaN; // Неправильно
