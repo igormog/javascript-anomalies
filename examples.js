245fd383f2c2162da40533d461a6a6429c5266d9
Igor Mogilin <i@igorm.org>
2015-08-31 23:46:53 +0300

    delete function Stack()

--- examples.js
+++ examples.js
@@ -19,27 +19,3 @@ var у = х + f
 x == NaN; // Неправильно
 х != х; // Правильно
 isNaN(x); // Правильно
-
-
-function Stack() {
-  //private
-  var stack = new Array();
-  var savetype = function (self, value) {
-    self.pushed_type = typeof value;
-    //self.pushed_type = stack[stack.length - 1]; - также работает
-  }
-
-  //public
-  this.pushed_type = undefined;
-  this.push = function (value) {
-    stack.push(value);
-    savetype(self, value);
-  }
-  this.pop = function () {
-    if (stack.length > 0) {
-      return stack.pop();
-    } else {
-      return null;
-    }
-  }
-}
